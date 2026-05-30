4bd0399291ecf57a9f7b9ffaefd080816ad00595
LizVillasmil <karenlizethqv@ufps.edu.co>
2026-05-30 18:36:44 -0500

    feat: add mock interview service skeleton

---
+++ src/services/mock_interview.py
@@ -0,0 +1,11 @@
+class MockInterviewService:
+
+    def __init__(self):
+        self.messages = []
+        self.transcript = []
+
+    def run_interactive(self, profile, job_description):
+        pass
+
+    def export_transcript(self):
+        pass
